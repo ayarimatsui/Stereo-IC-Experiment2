ddded9026a2ea6b064c372ea314353f29513966d
ayarimatsui <ayanapo9846@gmail.com>
2023-01-20 21:02:09 +0900

    no message

--- ArduinoControl/FanControl.cpp
+++ ArduinoControl/FanControl.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Arduino.h"
 #include "FanControl.h"
 
@@ -56,3 +57,63 @@ void FanControl::off() {
   analogWrite(fan4, 0);
   analogWrite(fan5, 0);
 }
+=======
+#include "Arduino.h"
+#include "FanControl.h"
+
+FanControl::FanControl() {
+  fan1 = 3;
+  fan2 = 5;
+  fan3 = 6;
+  fan4 = 9;
+  fan5 = 10;
+}
+
+void FanControl::fan1On(int duty) {
+  analogWrite(fan1, duty);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, 0);
+  analogWrite(fan5, 0);
+}
+
+void FanControl::fan2On(int duty) {
+  analogWrite(fan1, 0);
+  analogWrite(fan2, duty);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, 0);
+  analogWrite(fan5, 0);
+}
+
+void FanControl::fan3On(int duty) {
+  analogWrite(fan1, 0);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, duty);
+  analogWrite(fan4, 0);
+  analogWrite(fan5, 0);
+}
+
+void FanControl::fan4On(int duty) {
+  analogWrite(fan1, 0);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, duty);
+  analogWrite(fan5, 0);
+}
+
+void FanControl::fan5On(int duty) {
+  analogWrite(fan1, 0);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, 0);
+  analogWrite(fan5, duty);
+}
+
+void FanControl::off() {
+  analogWrite(fan1, 0);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, 0);
+  analogWrite(fan5, 0);
+}
+>>>>>>> e089bb7db33fb37cfc3facb48be4e2c05cdd71c3
--- ArduinoControl/FanControl.h
+++ ArduinoControl/FanControl.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 
 class FanControl{
@@ -16,3 +17,23 @@ private:
   int fan4;
   int fan5;
 };
+=======
+#pragma once
+
+class FanControl{
+public:
+  FanControl();
+  void fan1On(int);
+  void fan2On(int);
+  void fan3On(int);
+  void fan4On(int);
+  void fan5On(int);
+  void off();
+private:
+  int fan1;
+  int fan2;
+  int fan3;
+  int fan4;
+  int fan5;
+};
+>>>>>>> e089bb7db33fb37cfc3facb48be4e2c05cdd71c3
